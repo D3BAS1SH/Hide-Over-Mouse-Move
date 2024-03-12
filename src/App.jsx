2a1a71d1b2c5147e192cd11778efe7d2ea0b7808
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-03-13 01:53:28 +0530

    Task Completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.scss'
 import useMousePosition from './useMousePosition'
+import { backOut, motion } from 'framer-motion';
 function App() {
-  const {x,y}=useMousePosition;
+  const {x,y}=useMousePosition();
+  const [isHovered,setIsHovered] = useState(false)
+  const size=isHovered?500:40;
   return (
     <main className='main'>
-      <div className="mask">
-        <p>Visual desinger - with skills that haven't been replaced by A.I(yet)  - making good shit only if the paycheck is equall good.</p>
-      </div>
+      <motion.div className="mask"
+      animate={{
+        WebkitMaskPosition:`${x- size/2}px ${y- size/2}px`,
+        WebkitMaskSize:`${size}px`,
+      }}
+      transition={{type:'tween',ease:backOut}}>
+        <p onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}>Visual desinger - with skills that haven't been replaced by A.I(yet)  - making good shit only if the paycheck is equall good.</p>
+      </motion.div>
       <div className="body">
         <p>
           I'm a <span>selectively skilled</span> product designer with strong focus on producing high quality & impactful digital experience.
--- src/useMousePosition.jsx
+++ src/useMousePosition.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 const useMousePosition = () => {
-    const [mousePosition,setMousePosition]=useState({x:0,y:0});
-    useEffect(()=>{
-        const update = (e) =>{
-            setMousePosition({x:e.clientX,y:e.clientY});
-        }
-        window.addEventListener('mousemove',update)
-        return ()=>{
-            window.removeEventListener('mousemove',update)
-        }
-    },[mousePosition])
-  return (
-    <div>useMousePosition</div>
-  )
-}
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
 
-export default useMousePosition
+  
+  useEffect(() => {
+    console.log(mousePosition);
+      const updateMousePosition = e => {
+        setMousePosition({ x: e.clientX, y: e.clientY });
+      };
+      window.addEventListener("mousemove", updateMousePosition);
+
+    return () => window.removeEventListener("mousemove", updateMousePosition);
+  }, [mousePosition]);
+
+  return (mousePosition);
+};
+
+export default useMousePosition;
