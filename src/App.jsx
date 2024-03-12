531de05e474f49fd3f8a71f59d7e5255bb115eb8
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-03-13 01:08:27 +0530

    Initial Push with little bit of styling of 2 different paragraphs.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss'
+
+function App() {
+  return (
+    <main className='main'>
+      <div className="mask">
+        <p>Visual desinger - with skills that haven't been replaced by A.I(yet)  - making good shit only if the paycheck is equall good.</p>
+      </div>
+      <div className="body">
+        <p>
+          I'm a <span>selectively skilled</span> product designer with strong focus on producing high quality & impactful digital experience.
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default App
